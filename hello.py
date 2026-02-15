fa62955ed5e3e588d9d70dd8c73f8d53526eeb90
manoj Nayapura <manojnayapura20@gmail.com>
2026-02-15 19:48:07 +0530

    mera pahala python code

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
-print("relaince mode is on")
-print("relaince mode is on")
-print("relaince mode is on")
+city =str(input("enter your city"))
+age = int(input("enter your age"))
+print("tumhari city "+str(city)+" or age "+str(age)+" hai")
