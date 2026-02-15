605d3b94ef40e6d5f79dec650e560674df16ab3e
manoj Nayapura <manojnayapura20@gmail.com>
2026-02-15 20:01:07 +0530

    mera pahala python code

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-city =str(input("enter your city"))
-age = int(input("enter your age"))
-print("tumhari city "+str(city)+" or age "+str(age)+" hai")
+# city =str(input("enter your city"))
+# age = int(input("enter your age"))
+# print("tumhari city "+str(city)+" or age "+str(age)+" hai")
+maharanapratap =str(input("enter maharana pratap"))
+if maharanapratap == "pratap":
+    print("you are Like maharana")
+else:
+   print("your zero")
